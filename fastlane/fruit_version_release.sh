c5e8ff58a8ade0e3317ef633e3ec4929a0b04d68
Nazar Lenyk <nazar.lenyk@vanongo.com>
2023-12-25 21:10:36 +0200

    feat: extract version, fruit name, and build number from pubspec.yaml

--- fastlane/fruit_version_release.sh
+++ fastlane/fruit_version_release.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# increase_version.sh
-
 # Save the current directory and move to the root directory
 current_dir=$(pwd)
 cd ..
@@ -9,14 +7,14 @@ cd ..
 # Path to the fruits list in the root directory
 fruits_file="${current_dir}/fruits.txt"
 
-# Extract the current version and fruit name from pubspec.yaml
-current_version=$(grep 'version:' pubspec.yaml | sed 's/version: //' | sed 's/-.*//')
-current_fruit=$(grep 'version:' pubspec.yaml | sed 's/.*-//')
+# Extract the current version, fruit name, and build number from pubspec.yaml
+version_line=$(grep 'version:' pubspec.yaml)
+current_fruit=$(echo $version_line | sed 's/version: //' | sed 's/.*-//' | sed 's/+.*//')
+build_number=$(echo $version_line | sed 's/.*+//')
 
 # Increment the major version
-IFS='.' read -ra VERSION_PARTS <<< "$current_version"
-major=${VERSION_PARTS[0]}
-major=$((major + 1))
+current_major=$(echo $version_line | sed 's/version: //' | sed 's/\..*//')
+major=$((current_major + 1))
 
 # Get the next fruit name
 next_fruit=""
@@ -36,8 +34,8 @@ if [ -z "$next_fruit" ]; then
     next_fruit=$(head -n 1 $fruits_file)
 fi
 
-# Combine the new version and fruit name
-new_version="$major.0.0-$next_fruit"
+# Combine the new version, fruit name, and build number
+new_version="$major.0.0-$next_fruit+$build_number"
 
 # Update the version in pubspec.yaml
 sed -i '' "s/version: .*/version: $new_version/g" pubspec.yaml
