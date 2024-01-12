1b11a45bf60c4b863dce70dc27b5a9b622b6ca93
Nazar Lenyk <nazar.lenyk@vanongo.com>
2024-01-12 22:37:09 +0200

    feat: add post_site.dart with site content

---
+++ lib/app/features/main/screen/blog/post_site.dart
@@ -0,0 +1,27 @@
+import "package:onelenykco/app/features/main/screen/blog/blog_page.dart";
+
+final Note site = Note(note:
+"""
+# Welcome to My Developer's Hub: Where Flutter and Innovation Meet 🚀
+
+Hey everyone! I'm Nazar Lenyk, an enthusiastic Android Engineer, and this is my newly minted personal website. Crafted with Flutter, it's a fusion of modern web app dynamics and mobile app sensibilities. Let's dive into what makes my site special and what you can discover here:
+
+[#ff6b6b26](### Flutter's Web Capabilities Unleashed 🌐)
+Flutter isn't just for mobile apps; it's powering my website too! Here's a snapshot of the tech behind it:
+- **Dart Power:** The core language enabling Flutter to animate my site.
+- **State Management:** Smoothly handled by BLoC and Freezed for a responsive UI.
+- **Easy Navigation:** Thanks to AutoRoute, getting around the site is effortless.
+- **Blog Posts in Markdown:** While the site is versatile, the blog stands out with its Markdown-crafted posts, combining ease and elegance.
+
+[#ffa07a26](### Hosted, Deployed, and Delivered with Care 🔥)
+- **Firebase Hosting:** For a fast, secure user experience.
+- **Fastlane for Deployment:** Ensuring the site is always up-to-date effortlessly. Fastlane scripts automate the build and release process, making deployment a breeze.
+
+[#ffda7926](### Explore My Digital Realm 🧭)
+- **Resume:** A deep dive into my professional achievements and skills.
+- **Hire Me:** Why I could be your go-to person for Android development.
+- **Collaboration:** An open invitation for creative and technical partnerships.
+- **Blog:** A corner for sharing insights, stories, and tips from my Android engineering adventures.
+
+I'm thrilled to have you on board this digital journey! Stay tuned for updates, tech talk, and much more.
+""", id: "template", date: DateTime.parse("2024-01-12"));
