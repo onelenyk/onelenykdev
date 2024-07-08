f8fdd5ef74fbeef09e2d0558f58a07179c74bfe3
Nazar Lenyk <nazar.lenyk@vanongo.com>
2024-07-08 21:03:14 +0300

    fix: fix TerminalView class and related methods

--- lib/app/features/main/screen/tgchannel/terminal.dart
+++ lib/app/features/main/screen/tgchannel/terminal.dart
@@ -3,104 +3,34 @@ import "package:flutter_svg/svg.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:intl/intl.dart";
 import "package:onelenykco/app/features/main/screen/tgchannel/image_saver.dart";
+import "package:onelenykco/app/features/main/screen/tgchannel/what_is_it.dart";
 import "package:screenshot/screenshot.dart";
 
 import "../../../../common/hover_button.dart";
 import "gradle_post.dart";
 import "markdown_renderer.dart";
 
-class TerminalView extends StatefulWidget {
+class TerminalView extends StatelessWidget {
   final WhatIsItModel params;
-  final ScreenshotController screenshotController;
-  final Function(WhatIsItModel) onUpdate;
 
-  TerminalView({
+  const TerminalView({
+    Key? key,
     required this.params,
-    required this.screenshotController,
-    required this.onUpdate,
-  });
+  }) : super(key: key);
 
   @override
-  _TerminalViewState createState() => _TerminalViewState();
-}
-
-class _TerminalViewState extends State<TerminalView> {
-  late WhatIsItModel _currentParams;
-  bool _isModifying = false;
-  final TextEditingController _topicNameController = TextEditingController();
-  final TextEditingController _contentTextController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _currentParams = widget.params;
-    _topicNameController.text = _currentParams.topicName;
-    _contentTextController.text = _currentParams.contentText;
-  }
-
-  void _toggleModifyingMode() {
-    setState(() {
-      _isModifying = !_isModifying;
-      if (!_isModifying) {
-        // Save changes and update parameters
-        _currentParams = _currentParams.copyWith(
-          topicName: _topicNameController.text,
-          contentText: _contentTextController.text,
-        );
-        widget.onUpdate(_currentParams);
-      }
-    });
-  }
-
-  String getCurrentTimeFormatted() {
-    DateTime now = DateTime.now();
-    return DateFormat('dd-MM-yyyy').format(now);
-  }
-
-  Future<void> _doScreenshot(BuildContext context) async {
-    try {
-      final capturedImage = await widget.screenshotController
-          .capture(delay: const Duration(milliseconds: 10));
-      final name = "telegram_post_${_currentParams.id}.png";
-      if (capturedImage != null) {
-        await ImageSaver.saveImage(capturedImage, name);
-        await ImageSaver.showCapturedWidget(context, capturedImage);
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) => Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _buildIndex(context),
-          Screenshot(
-            controller: widget.screenshotController,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: _buildTerminalContainer()),
-          ),
-          _buildScreenshotButton(context),
-        ],
-      );
-
-  Widget _buildTerminalContainer() => Container(
+  Widget build(BuildContext context) => Container(
         decoration: BoxDecoration(
-          color: _currentParams.backgroundColor,
-          border: Border.all(
-            color: _currentParams.backgroundColor,
-          ),
+          color: params.backgroundColor,
+          border: Border.all(color: params.backgroundColor),
           borderRadius: BorderRadius.circular(8),
         ),
-        padding: _currentParams.backgroundPadding,
+        padding: params.backgroundPadding,
         child: Container(
           width: 430,
           height: 400,
           decoration: BoxDecoration(
-            color: _currentParams.terminalBgColor,
+            color: params.terminalBgColor,
             borderRadius: BorderRadius.circular(8),
             boxShadow: [
               BoxShadow(
@@ -116,34 +46,15 @@ class _TerminalViewState extends State<TerminalView> {
               mainAxisSize: MainAxisSize.max,
               children: [
                 _buildTerminalHeader(),
-                Divider(color: _currentParams.separatorColor),
-                GestureDetector(
-                  onDoubleTap: _toggleModifyingMode,
-                  child: _isModifying
-                      ? _buildEditableTopicTitle()
-                      : _buildTopicTitle(),
-                ),
-                Divider(color: _currentParams.separatorColor),
-                _isModifying
-                    ? _buildEditableContentText()
-                    : _buildMarkdownContentText(),
+                Divider(color: params.separatorColor),
+                _buildTopicTitle(),
+                Divider(color: params.separatorColor),
+                _buildMarkdownContentText(),
                 Spacer(),
-                Divider(color: _currentParams.separatorColor),
+                Divider(color: params.separatorColor),
                 _buildTelegramChannelName(),
               ],
             ),
-          ),
-        ),
-      );
-
-  Widget _buildTelegramChannelName() => Padding(
-        padding: const EdgeInsets.only(top: 8.0),
-        child: Text(
-          '@dailycodefreestyle',
-          style: GoogleFonts.robotoMono(
-            fontSize: 14,
-            fontWeight: FontWeight.normal,
-            color: _currentParams.contentColor,
           ),
         ),
       );
@@ -155,9 +66,9 @@ class _TerminalViewState extends State<TerminalView> {
           children: [
             Row(
               children: [
-                _buildButton(_currentParams.buttonRed),
-                _buildButton(_currentParams.buttonYellow),
-                _buildButton(_currentParams.buttonGreen),
+                _buildButton(params.buttonRed),
+                _buildButton(params.buttonYellow),
+                _buildButton(params.buttonGreen),
               ],
             ),
             Row(
@@ -183,35 +94,240 @@ class _TerminalViewState extends State<TerminalView> {
       );
 
   Widget _buildTopicTitle() => Container(
-        color: _currentParams.titleBgColor,
+        color: params.titleBgColor,
         child: Row(
           children: [
             const SizedBox(width: 8),
             SvgPicture.network(
-              _currentParams.topicIcon,
+              params.topicIcon,
               width: 24,
               height: 24,
             ),
             const SizedBox(width: 8),
             Text(
-              _currentParams.topicName,
+              params.topicName,
               style: GoogleFonts.robotoMono(
                 fontSize: 24,
                 fontWeight: FontWeight.normal,
-                color: _currentParams.titleColor,
+                color: params.titleColor,
               ),
             ),
           ],
         ),
       );
 
+  Widget _buildMarkdownContentText() => Container(
+        color: params.contentBgColor,
+        child: MarkdownRenderer(
+          data: params.contentText,
+          baseTextStyle: GoogleFonts.robotoMono(
+            fontSize: 16,
+            fontWeight: FontWeight.normal,
+            color: params.contentColor,
+          ),
+        ),
+      );
+
+  Widget _buildTelegramChannelName() => Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: Text(
+          '@dailycodefreestyle',
+          style: GoogleFonts.robotoMono(
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+            color: params.contentColor,
+          ),
+        ),
+      );
+
+  Widget _buildButton(Color color) => Container(
+        margin: const EdgeInsets.only(right: 5),
+        width: 12,
+        height: 12,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
+      );
+}
+
+class TerminalEditor extends StatefulWidget {
+  final WhatIsItModel params;
+  final Function(WhatIsItModel) onUpdate;
+  final Function() onCancel;
+
+  const TerminalEditor({
+    Key? key,
+    required this.params,
+    required this.onUpdate,
+    required this.onCancel,
+  }) : super(key: key);
+
+  @override
+  _TerminalEditorState createState() => _TerminalEditorState();
+}
+
+class _TerminalEditorState extends State<TerminalEditor> {
+  late WhatIsItModel _tempParams;
+  final TextEditingController _topicNameController = TextEditingController();
+  final TextEditingController _contentTextController = TextEditingController();
+
+  @override
+  void didUpdateWidget(TerminalEditor oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.params != widget.params) {
+      setState(() {
+        _tempParams = widget.params;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tempParams = widget.params;
+
+    _topicNameController.text = _tempParams.topicName;
+    _contentTextController.text = _tempParams.contentText;
+  }
+
+  void _saveChanges() {
+    setState(() {
+      _tempParams = _tempParams.copyWith(
+        topicName: _topicNameController.text,
+        contentText: _contentTextController.text,
+      );
+      widget.onUpdate(_tempParams);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Column(
+    children: [
+      Container(
+            decoration: BoxDecoration(
+              color: _tempParams.backgroundColor,
+              border: Border.all(color: _tempParams.backgroundColor),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            padding: _tempParams.backgroundPadding,
+            child: Container(
+              width: 430,
+              height: 400,
+              decoration: BoxDecoration(
+                color: _tempParams.terminalBgColor,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 10,
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    _buildTerminalHeader(),
+                    Divider(color: _tempParams.separatorColor),
+                    _buildEditableTopicTitle(),
+                    Divider(color: _tempParams.separatorColor),
+                    _buildEditableContentText(),
+                    Spacer(),
+                    Divider(color: _tempParams.separatorColor),
+                    _buildTelegramChannelName(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+      SizedBox(
+        height: 8,
+      ),
+      Row(
+        children: [
+          HoverButton(
+            onTap: widget.onCancel,
+            onDoubleTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                "cancel",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.redAccent.shade200,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 8,
+          ),
+          HoverButton(
+            onTap: _saveChanges,
+            onDoubleTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                "save",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.redAccent.shade200,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+
+  Widget _buildTerminalHeader() => Container(
+        padding: const EdgeInsets.all(0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                _buildButton(_tempParams.buttonRed),
+                _buildButton(_tempParams.buttonYellow),
+                _buildButton(_tempParams.buttonGreen),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 16,
+                  height: 16,
+                  child: SvgPicture.asset("svg/dailycodefreestyle.svg"),
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  'What is it',
+                  style: GoogleFonts.robotoMono(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+
   Widget _buildEditableTopicTitle() => Container(
-        color: _currentParams.titleBgColor,
+        color: _tempParams.titleBgColor,
         child: Row(
           children: [
             const SizedBox(width: 8),
             SvgPicture.network(
-              _currentParams.topicIcon,
+              _tempParams.topicIcon,
               width: 24,
               height: 24,
             ),
